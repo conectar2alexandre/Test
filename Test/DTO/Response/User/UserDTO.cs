28e30f0dc790c00d07372db20064de131a5e3634
conectar2.alexandre <conectar2.alexandre@gmail.com>
2025-05-30 15:52:23 -0300

    userDTO

---
+++ Test/DTO/Response/User/UserDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Test.DTO.Out.User
+{
+        public class UserDTO
+        {
+            public string Login { get; set; }
+
+            public string Email { get; set; }
+        }
+
+}
